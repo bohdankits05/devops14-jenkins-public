59f9f36ef092e286406b97862f32df4754e01811
bohdankits05 <88409994+bohdankits05@users.noreply.github.com>
2021-10-17 10:54:55 -0400

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3.34.0,<=3.35.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-2"
+  #shared_credentials_file = “/var/lib/jenkins/.aws/credentials”
+  profile = "default"
+}
+
+resource "aws_instance” “my-first-ec2" {
+  ami           = "ami-074cce78125f09d61"
+  instance_type = "t2.micro"
+  tags = {
+    Name  = "myec2-1"
+    Owner = "Bohdan"
+  }
+}
